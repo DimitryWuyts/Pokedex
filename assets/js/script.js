ffa30d1a9cc3661fddfc9d1f4ff36de8d46ce08e
Chihyu <chihyu10@gmail.com>
2020-01-15 15:04:34 +0100

    Progress on getting the right information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-https://pokeapi.co/api/v2/pokemon
+
+document.getElementById("midCircle").addEventListener("click",function () {
+    console.log("mouseclick");
+
+    let pokemon = document.getElementById("input_field").value;
+    let pokemonData = [];
+    axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemon + "/")
+        .then(function (response) {
+            pokemonData = response.data.abilities;
+            console.log(pokemonData);
+            console.log(response)
+        });
+
+
+
+
+
+});
