98235d5906e48a4f0e2ba298a6eb0c199b1316bc
Chihyu <chihyu10@gmail.com>
2020-01-16 16:21:50 +0100

    Previous evolution kinda working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,12 @@ document.getElementById("midCircle").addEventListener("click",function () {
     let pokemon = document.getElementById("input_field");
     let pokemonData = [];
     axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemon.value.toLowerCase() + "/")
-        .then(function (response) {
-            pokemonData = response.data;
+        .then(function (Pokedata) {
+            pokemonData = Pokedata.data;
 
 
+
+            console.log(Pokedata)
             //Get sprites from API
            let sprites = document.getElementById("sprite");
             sprites.src = pokemonData.sprites.front_default;
@@ -36,8 +38,6 @@ document.getElementById("midCircle").addEventListener("click",function () {
 
             let moveList = [];
             moveList.push(pokemonData.moves);
-
-
             for (let i = 0; i < 4 ; i++) {
                 let rndNumber = randomizer();
                 randomizer(moveList.length);
@@ -46,13 +46,44 @@ document.getElementById("midCircle").addEventListener("click",function () {
 
                 document.getElementById("move" +(i + 1 )+ '').innerHTML = pokemonData.moves[rndNumber].move.name;
 
+                console.log(pokemonData.species.url)
             }
 
         });
 
 
+          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.value.toLowerCase()}`)
+              .then(function (response) {
+                  console.log(response);
+
+                  let prevName = response.data.evolves_from_species.name;
+
+                    console.log(prevName)
 
 
+
+                  axios.get(`https://pokeapi.co/api/v2/pokemon/${prevName}/`)
+                    .then(function (antwoord) {
+                        console.log(antwoord)
+                        let prevEvolSprite = document.getElementById("evolPrev");
+                        prevEvolSprite.src = antwoord.data.sprites.front_default;
+
+                    });
+              });
+
+
+
+
+
+
+    // axios.get(pokemonData.species.url)
+    //     .then(function(response) {
+    //     return response.json();
+    // });
+
+
+
+    //Randomizer function
     function randomizer() {
         return Math.floor(Math.random() * 50);
     }
