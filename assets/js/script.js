59694e724b018cb87110be6813cee0a231602fff
Chihyu <chihyu10@gmail.com>
2020-01-16 10:47:18 +0100

    Almost working randomizer for moves

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@
 document.getElementById("midCircle").addEventListener("click",function () {
 
 
-    let pokemon = document.getElementById("input_field").value;
+    let pokemon = document.getElementById("input_field");
     let pokemonData = [];
-    axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemon + "/")
+    axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemon.value.toLowerCase() + "/")
         .then(function (response) {
             pokemonData = response.data;
 
@@ -20,16 +20,35 @@ document.getElementById("midCircle").addEventListener("click",function () {
             let poknr = pokemonData.id;
             document.getElementById("pokNr").innerHTML = "ID: " + poknr;
 
+
+            // Get pokemon name from API
             let pokName = pokemonData.name;
             document.getElementById("pokName").innerHTML = "Name: " + pokName;
 
+            let moveList = [];
 
 
-            console.log(response)
+            moveList.push(pokemonData.moves);
+
+
+            for (let i = 0; i < 4 ; i++) {
+                let rndNumber = randomizer();
+                randomizer(moveList.length);
+                console.log(pokemonData.moves[rndNumber].move.name);
+
+                console.log(rndNumber)
+
+
+            }
 
         });
 
 
+
+
+    function randomizer() {
+        return Math.floor(Math.random() *100);
+    }
 
 
 
